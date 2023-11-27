10c24adb359290efeb643747d95f1fcf3a19af2b
Byron Rodrigo <byrodg@gmail.com>
2023-11-26 23:30:24 -0500

    FEATURE: Managing dependencies for build

--- com_yogomatt_sensor_collection.egg-info/dht22reader.py
+++ com_yogomatt_sensor_collection.egg-info/dht22reader.py
@@ -5,8 +5,8 @@ import adafruit_dht
 import requests
 
 # Constants
-TEMPERATURE_REST_API_URL = 'http://192.168.0.102:8081/temperature'
-HUMIDITY_REST_API_URL = 'http://192.168.0.102:8081/humidity'
+TEMPERATURE_REST_API_URL = 'http://192.168.0.160:8080/api/measure'
+HUMIDITY_REST_API_URL = 'http://192.168.0.160:8080/api/measure'
 
 # Initiate the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT22(board.D22, use_pulseio=False)
@@ -18,7 +18,7 @@ dhtDevice = adafruit_dht.DHT22(board.D22, use_pulseio=False)
 
 # Init the cvs file
 try:
-    file_path = './logs/{0}.csv'.format(time.strftime('%y-%m-%d'))
+    file_path = '/home/byron/my_logs/{0}.csv'.format(time.strftime('%y-%m-%d'))
     f = open(file_path, 'a+')
     if os.stat(file_path).st_size == 0:
         f.write('Date,Time,Temperature,Humidity\r\n')
@@ -29,44 +29,52 @@ except Exception as error:
 while True:
     try:
         # Print the values to the serial port
+        sample_time = time.strftime('%Y-%m-%dT%H:%M:%S')
         temperature_c = dhtDevice.temperature
         temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
         print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
+            "Time: {}  Temp: {:.1f} F / {:.1f} C  Humidity: {}% ".format(
+                sample_time, temperature_f, temperature_c, humidity
             )
         )
 
         # Store in a cvs file
         if temperature_c is not None and humidity is not None:
-            f.write('{0} {1} {2:0.1f} C {3:0.1f}\r\n'.format(time.strftime('%Y/%m/%d'),
-                time.strftime('%H:%M:%S'), temperature_c, humidity))
+            f.write('{0} {1:0.1f} C {2:0.1f}\r\n'.format(sample_time,
+                temperature_c, humidity))
         else:
             print('Failed to retrieve the sensor data')
 
         # Store in the "cloud"
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        # headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        headers = {'Content-Type': 'application/json'}
 
         sampleTemperature = {
-            "sampleDate": time.strftime('%Y-%m-%d'),
-            "sampleTime": time.strftime('%H:%M:%S'),
-            "device": "DHT22",
-            "valueCentigrades": str(temperature_c)
+            "sampleDate": sample_time,
+            "sampleType": "temperature",
+            "deviceId": "DHT22",
+  	    "measure": {
+		"type": "centigrades",
+            	"value": str(temperature_c)
+	    }
         }
 
-        resp = requests.post(TEMPERATURE_REST_API_URL, data = sampleTemperature, headers = headers)
+        resp = requests.post(TEMPERATURE_REST_API_URL, json = sampleTemperature, headers = headers)
 
         print(resp)
 
         sampleHumidity = {
-            "sampleDate": time.strftime('%Y-%m-%d'),
-            "sampleTime": time.strftime('%H:%M:%S'),
-            "device": "DHT22",
-            "value": str(humidity)
+            "sampleDate": sample_time,
+            "sampleType": "humidity",
+            "deviceId": "DHT22",
+            "measure": {
+		"type": "percentage",
+           	"value": str(humidity)
+            }
         }
 
-        resp = requests.post(HUMIDITY_REST_API_URL, data = sampleHumidity, headers = headers)
+        resp = requests.post(HUMIDITY_REST_API_URL, json = sampleHumidity, headers = headers)
 
         print(resp)
     except RuntimeError as error:
